0485f0ff9892cb466f7f00871a8a1c995cd3364f
Eric Wu <ericchunyuwu@gmail.com>
2024-10-26 22:16:28 +0800

    Revert "test path setting"
    This reverts commit 0aa212e50d45e3c84f7e8149550274885dcf3e1c.

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: '/MyResume/',
-//   plugins: [vue()],
-// })
-
-
-module.exports = {
-  publicPath: '/MyResume/'
-}
+export default defineConfig({
+  base: '/MyResume/',
+  plugins: [vue()],
+})
