0aa212e50d45e3c84f7e8149550274885dcf3e1c
Eric Wu <ericchunyuwu@gmail.com>
2024-10-26 22:06:47 +0800

    test path setting

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/MyResume/',
-  plugins: [vue()],
-})
+// export default defineConfig({
+//   base: '/MyResume/',
+//   plugins: [vue()],
+// })
+
+
+module.exports = {
+  publicPath: '/MyResume/'
+}
